a0ddb658b1a4e60890f1fb613354a7d2bae10ca3
Trung Đỗ <hou.dobaotrung@gmail.com>
2021-08-22 18:55:06 +0700

    Fix helper error

--- plugins/ahmadfatoni/apigenerator/helpers/Helpers.php
+++ plugins/ahmadfatoni/apigenerator/helpers/Helpers.php
@@ -1,23 +1,26 @@
-<?php namespace AhmadFatoni\ApiGenerator\Helpers;
+<?php
+
+namespace AhmadFatoni\ApiGenerator\Helpers;
 
 use Illuminate\Pagination\LengthAwarePaginator;
 
-Class Helpers {
+class Helpers
+{
 
-	public function apiArrayResponseBuilder($statusCode = null, $message = null, $data = [])
-	{
-		$arr = [
-			'status_code' => (isset($statusCode)) ? $statusCode : 500,
-			'message' => (isset($message)) ? $message : 'error'
-		];
-		if (count($data) > 0 && !$data instanceof LengthAwarePaginator) {
-			$arr['data'] = $data;
-		} else {
-		    return array_merge($arr, $data->toArray());
+    public function apiArrayResponseBuilder($statusCode = null, $message = null, $data = [])
+    {
+        $arr = [
+            'status_code' => (isset($statusCode)) ? $statusCode : 500,
+            'message' => (isset($message)) ? $message : 'error'
+        ];
+        if (count($data) > 0 && !$data instanceof LengthAwarePaginator) {
+            $arr['data'] = $data;
+        } else {
+            return response()->json(array_merge($arr, !empty($data) ? $data->toArray() : []), $arr['status_code']);
         }
 
-		return response()->json($arr, $arr['status_code']);
-		//return $arr;
+        return response()->json($arr, $arr['status_code']);
+        //return $arr;
 
-	}
+    }
 }
