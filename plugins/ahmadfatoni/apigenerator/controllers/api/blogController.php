1b23ed78e8f9635c94ea4e5f58c9f95b7e33ff55
Trung Đỗ <hou.dobaotrung@gmail.com>
2021-08-26 02:16:25 +0700

    Them anh cho blog

--- plugins/ahmadfatoni/apigenerator/controllers/api/blogController.php
+++ plugins/ahmadfatoni/apigenerator/controllers/api/blogController.php
@@ -1,4 +1,6 @@
-<?php namespace AhmadFatoni\ApiGenerator\Controllers\API;
+<?php
+
+namespace AhmadFatoni\ApiGenerator\Controllers\API;
 
 use Cms\Classes\Controller;
 use BackendMenu;
@@ -7,9 +9,10 @@ use Illuminate\Http\Request;
 use AhmadFatoni\ApiGenerator\Helpers\Helpers;
 use Illuminate\Support\Facades\Validator;
 use RainLab\Blog\Models\Post;
+
 class blogController extends Controller
 {
-	protected $Post;
+    protected $Post;
 
     protected $helpers;
 
@@ -27,80 +30,80 @@ class blogController extends Controller
         return $this->helpers->apiArrayResponseBuilder(200, 'success', $data->toArray());
     }
 
-    public function index(){
-
-        $data = $this->Post->with('categories')->get()->toArray();
+    public function index()
+    {
+        $data = $this->Post->with('categories')->with('featured_images')->get()->toArray();
 
         return $this->helpers->apiArrayResponseBuilder(200, 'success', $data);
     }
 
-    public function show($id){
+    public function show($id)
+    {
 
-        $data = $this->Post::find($id);
+        $data = $this->Post->with('categories')->with('featured_images')->find($id);
 
-        if ($data){
+        if ($data) {
             return $this->helpers->apiArrayResponseBuilder(200, 'success', [$data]);
         } else {
             $this->helpers->apiArrayResponseBuilder(404, 'not found', ['error' => 'Resource id=' . $id . ' could not be found']);
         }
-
     }
-//
-//    public function store(Request $request){
-//
-//    	$arr = $request->all();
-//
-//        while ( $data = current($arr)) {
-//            $this->Post->{key($arr)} = $data;
-//            next($arr);
-//        }
-//
-//        $validation = Validator::make($request->all(), $this->Post->rules);
-//
-//        if( $validation->passes() ){
-//            $this->Post->save();
-//            return $this->helpers->apiArrayResponseBuilder(201, 'created', ['id' => $this->Post->id]);
-//        }else{
-//            return $this->helpers->apiArrayResponseBuilder(400, 'fail', $validation->errors() );
-//        }
-//
-//    }
-//
-//    public function update($id, Request $request){
-//
-//        $status = $this->Post->where('id',$id)->update($data);
-//
-//        if( $status ){
-//
-//            return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been updated successfully.');
-//
-//        }else{
-//
-//            return $this->helpers->apiArrayResponseBuilder(400, 'bad request', 'Error, data failed to update.');
-//
-//        }
-//    }
-//
-//    public function delete($id){
-//
-//        $this->Post->where('id',$id)->delete();
-//
-//        return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been deleted successfully.');
-//    }
-//
-//    public function destroy($id){
-//
-//        $this->Post->where('id',$id)->delete();
-//
-//        return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been deleted successfully.');
-//    }
-//
-//
-//    public static function getAfterFilters() {return [];}
-//    public static function getBeforeFilters() {return [];}
-//    public static function getMiddleware() {return [];}
-//    public function callAction($method, $parameters=false) {
-//        return call_user_func_array(array($this, $method), $parameters);
-//    }
+    //
+    //    public function store(Request $request){
+    //
+    //    	$arr = $request->all();
+    //
+    //        while ( $data = current($arr)) {
+    //            $this->Post->{key($arr)} = $data;
+    //            next($arr);
+    //        }
+    //
+    //        $validation = Validator::make($request->all(), $this->Post->rules);
+    //
+    //        if( $validation->passes() ){
+    //            $this->Post->save();
+    //            return $this->helpers->apiArrayResponseBuilder(201, 'created', ['id' => $this->Post->id]);
+    //        }else{
+    //            return $this->helpers->apiArrayResponseBuilder(400, 'fail', $validation->errors() );
+    //        }
+    //
+    //    }
+    //
+    //    public function update($id, Request $request){
+    //
+    //        $status = $this->Post->where('id',$id)->update($data);
+    //
+    //        if( $status ){
+    //
+    //            return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been updated successfully.');
+    //
+    //        }else{
+    //
+    //            return $this->helpers->apiArrayResponseBuilder(400, 'bad request', 'Error, data failed to update.');
+    //
+    //        }
+    //    }
+    //
+    //    public function delete($id){
+    //
+    //        $this->Post->where('id',$id)->delete();
+    //
+    //        return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been deleted successfully.');
+    //    }
+    //
+    //    public function destroy($id){
+    //
+    //        $this->Post->where('id',$id)->delete();
+    //
+    //        return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been deleted successfully.');
+    //    }
+    //
+    //
+    //    public static function getAfterFilters() {return [];}
+    //    public static function getBeforeFilters() {return [];}
+    //    public static function getMiddleware() {return [];}
+    //    public function callAction($method, $parameters=false) {
+    //        return call_user_func_array(array($this, $method), $parameters);
+    //    }
 
 }
